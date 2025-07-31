7500c9d728c2d5c830af6f4bd206f1eef5f7f0e6
Kaneki-ken009 <Anakhutankesayangan.3@gmail.com>
2025-07-31 09:25:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restoran 😋</title>
+</head>
+<body>
+    <h1>hello word 🌍</h1>
+</body>
+</html>
